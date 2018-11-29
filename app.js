f556eca97cc25b92162bc6e551037bc2f0f894e0
ashudeo4 <ashudeo4@gmail.com>
2018-11-29 15:29:11 +0530

    handled auth

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+require('./config/passport')(passport);
+const auth = require('./routes/auth');
+
 
 const app = express();
 
-
 const port = process.env.PORT || 3000;
-
+app.use('/auth', auth);
 app.get('/', (req, res) => {
     res.send('ok');
 })
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://user:qwerty123@ds119734.mlab.com:19734/storybooks',
+    googleClientID: "924907361131-9v0g4stq9sg1lgtut1e6eqaop2tughi3.apps.googleusercontent.com",
+    googleClientSecret: '7KeOdcZ4jlTR2aaY-2AfjfQE'
+}
