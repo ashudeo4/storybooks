263bad065be126d68d57d06324e3955960c5f141
ashudeo4 <ashudeo4@gmail.com>
2018-11-29 16:32:37 +0530

    auth configured

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const keys = require('./keys');
+const User = require('../models/user');
 
 module.exports = function(passport) {
     passport.use(
@@ -11,10 +12,36 @@ module.exports = function(passport) {
                 proxy: true
             },
             function(accessToken, refreshToken, profile, done) {
-                console.log(accessToken);
-                console.log(profile);
+                // console.log(accessToken);
+                // console.log(profile);
+                const image = profile.photos[0].value.substring(0, profile.photos[0].value.indexOf('?'));
+                // console.log(image);
+                const newUser = {
+                    googleID: profile.id,
+                    firstName: profile.name.givenName,
+                    lastName: profile.name.familyName,
+                    email: profile.emails[0].value,
+                    image: image
+                }
 
-
+                //check for existing user
+                User.findOne({
+                    googleID: profile.id
+                }).then(user => {
+                    if (user) {
+                        done(null, user);
+                    } else {
+                        new User(newUser).save().then(user => {
+                            done(null, user);
+                        });
+                    }
+                });
             }
         ));
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+    passport.deserializeUser((id, done) => {
+        User.findById(id).then(user => done(null, user));
+    });
 }
